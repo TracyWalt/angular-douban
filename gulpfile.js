b8fa0126268443eb5f0c155f4386da9c195a0770
yzf <1443299799@qq.com>
2016-03-18 15:50:56 +0800

    自动化启动

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+// 编译Sass
+gulp.task('sass', function() {
+    gulp.src('./app/css/scss/*.scss')
+        .pipe(sass())
+        .pipe(rename('index.css'))
+        .pipe(gulp.dest('./app/css/'));
+});
+
+
+// 默认任务
+gulp.task('default', function(){
+    // 监听任务 运行语句 gulp watch
+    gulp.watch('./app/css/scss/*.scss', ['sass']);
+});
