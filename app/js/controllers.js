2d4f70d05fc268d22d4919e8ef126db569934a81
yzf <1443299799@qq.com>
2016-03-22 11:33:37 +0800

    修复bug

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,12 +26,11 @@ angularDoubanControllers.controller('listCtrl', ['$scope','Base','$timeout',
         $timeout(function(){
             $('#list li').each(function(index){
                 $(this).css({
-                    'opacity':1,
                     'position':'absolute',
                     'left':108*(index%5)+15*(index%5)+'px',
                     'top':177*(Math.floor(index/5))+Math.floor(index/5)*15+'px'
                 });
-            }).parent().css('height',Math.ceil($('#list li').length/5)*192);
+            }).parent().css('height',Math.ceil($('#list li').length/5)*192).css('opacity',1);
         },0);
 
     });
